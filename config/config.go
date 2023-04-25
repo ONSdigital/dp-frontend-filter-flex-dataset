96f3bf549639edb13e1056ec9220144cbce76b6a
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2023-04-25 16:43:26 +0100

    Tidy env vars

--- config/config.go
+++ config/config.go
@@ -8,18 +8,18 @@ import (
 
 // Config represents service configuration for dp-frontend-filter-flex-dataset
 type Config struct {
-	Debug                       bool          `envconfig:"DEBUG"`
-	EnableMultivariate          bool          `envconfig:"ENABLE_MULTIVARIATE"`
-	EnableCustomSort            bool          `envconfig:"ENABLE_CUSTOM_SORT"`
-	BindAddr                    string        `envconfig:"BIND_ADDR"`
 	APIRouterURL                string        `envconfig:"API_ROUTER_URL"`
+	BindAddr                    string        `envconfig:"BIND_ADDR"`
+	Debug                       bool          `envconfig:"DEBUG"`
 	DefaultMaximumSearchResults int           `envconfig:"DEFAULT_MAXIMUM_SEARCH_RESULTS"`
-	PatternLibraryAssetsPath    string        `envconfig:"PATTERN_LIBRARY_ASSETS_PATH"`
-	SupportedLanguages          []string      `envconfig:"SUPPORTED_LANGUAGES"`
-	SiteDomain                  string        `envconfig:"SITE_DOMAIN"`
+	EnableCustomSort            bool          `envconfig:"ENABLE_CUSTOM_SORT"`
+	EnableMultivariate          bool          `envconfig:"ENABLE_MULTIVARIATE"`
 	GracefulShutdownTimeout     time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
 	HealthCheckInterval         time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
 	HealthCheckCriticalTimeout  time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
+	PatternLibraryAssetsPath    string        `envconfig:"PATTERN_LIBRARY_ASSETS_PATH"`
+	SiteDomain                  string        `envconfig:"SITE_DOMAIN"`
+	SupportedLanguages          []string      `envconfig:"SUPPORTED_LANGUAGES"`
 }
 
 var cfg *Config
@@ -47,17 +47,17 @@ func get() (*Config, error) {
 	}
 
 	cfg = &Config{
-		Debug:                       false,
-		EnableMultivariate:          false,
-		EnableCustomSort:            false,
-		BindAddr:                    "localhost:20100",
 		APIRouterURL:                "http://localhost:23200/v1",
+		BindAddr:                    "localhost:20100",
+		Debug:                       false,
 		DefaultMaximumSearchResults: 50,
-		SupportedLanguages:          []string{"en", "cy"},
-		SiteDomain:                  "localhost",
+		EnableCustomSort:            false,
+		EnableMultivariate:          false,
 		GracefulShutdownTimeout:     5 * time.Second,
 		HealthCheckInterval:         30 * time.Second,
 		HealthCheckCriticalTimeout:  90 * time.Second,
+		SiteDomain:                  "localhost",
+		SupportedLanguages:          []string{"en", "cy"},
 	}
 
 	return cfg, envconfig.Process("", cfg)
