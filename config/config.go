b7528b6a6c17a28aebeb558f1f8c879158c8dcbc
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2023-09-26 12:29:17 +0100

    1837 update to latest dp-design-system

--- config/config.go
+++ config/config.go
@@ -34,7 +34,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/0c107a5"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/3f77ec1"
 	}
 
 	return cfg, nil
